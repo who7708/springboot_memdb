05609d410e7eb4e974daf902bfd094e33860cc16
jerry <309032663@qq.com>
2021-09-06 13:11:20 +0800

    update

--- springboot-sqlite/src/main/java/cn/hsp/blog/BlogMapper.java
+++ springboot-sqlite/src/main/java/cn/hsp/blog/BlogMapper.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 /**
  * @author 花生皮编程(CSDN、简书、掘金、今日头条、微信公众号、抖音、快手、B站、西瓜视频)
+ * @关于作者 https://juejin.cn/post/7002792005688360968
  */
 @Repository
 public interface BlogMapper {
