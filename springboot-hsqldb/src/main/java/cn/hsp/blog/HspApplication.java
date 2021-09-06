93349035b7f6eeec1f148a7fb91867232738865a
jerry <309032663@qq.com>
2021-09-06 13:11:36 +0800

    update

--- springboot-hsqldb/src/main/java/cn/hsp/blog/HspApplication.java
+++ springboot-hsqldb/src/main/java/cn/hsp/blog/HspApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author 花生皮编程(CSDN、简书、掘金、今日头条、微信公众号、抖音、快手、B站、西瓜视频)
+ * @关于作者 https://juejin.cn/post/7002792005688360968
  */
 @MapperScan({"cn.hsp.blog"})
 @SpringBootApplication
